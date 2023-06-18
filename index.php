d128796d60199c7c342b89711fbd59dcb12fed57
Egemen Düren <136931557+egolasoa@users.noreply.github.com>
2023-06-18 13:46:36 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tarif Sitesi</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <header>
+        <h1>Tarif Sitesi</h1>
+    </header>
+
+    <nav>
+        <ul>
+            <li><a href="index.php">Anasayfa</a></li>
+            <li><a href="tarifler.php">Tarifler</a></li>
+            <li><a href="tarif-ekle.php">Tarif Ekle</a></li>
+            <li><a href="admin-paneli.php">Admin Paneli</a></li>
+        </ul>
+    </nav>
+
+    <main>
+        <h2>Hoş Geldiniz!</h2>
+        <p>En lezzetli tarifleri keşfetmek için tarifler sayfasını ziyaret edin.</p>
+    </main>
+
+    <footer>
+        &copy; 2023 Tarif Sitesi
+    </footer>
+</body>
+
+</html>
