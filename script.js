bc8629cd145a55dfc5d19654dfba9abac1002649
Egemen Düren <136931557+egolasoa@users.noreply.github.com>
2023-06-18 13:51:17 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// Tarif Ekleme Formu Gönderildiğinde
+document.getElementById('tarif-ekle-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    // Tarif ekleme işlemleri burada yapılabilir
+});
